c62ff125a908311683fa969ed9bee638aa8e44f0
Anna Manole <am224wd@student.lnu.se>
2024-12-20 16:25:28 +0100

    change blogs to articles in frontend

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -16,23 +16,23 @@ interface TreeNode {
 }
 
 function App() {
-  const [blogs, setBlogs] = useState<string[]>([]);
+  const [articles, setArticles] = useState<string[]>([]);
   const [clusters, setClusters] = useState<Cluster>({});
   const [isLoadingFixed, setIsLoadingFixed] = useState<boolean>(false);
   const [isLoadingFlexible, setIsLoadingFlexible] = useState<boolean>(false);
   const [treeData, setTreeData] = useState<TreeNode[]>([]);
 
-  const fetchBlogTitles = async () => {
+  const fetchArticleTitles = async () => {
     const response = await fetch("http://localhost:3000/articles");
     const data = await response.json();
-    setBlogs(data);
+    setArticles(data);
   };
 
   useEffect(() => {
-    fetchBlogTitles();
+    fetchArticleTitles();
   }, []);
 
-  const clusterBlogsFixedIterations = async () => {
+  const clusterArticlesFixedIterations = async () => {
     if (isLoadingFixed) return;
     setIsLoadingFixed(true);
     setTreeData([]);
@@ -42,7 +42,7 @@ function App() {
     setClusters(data);
   };
 
-  const clusterBlogsFlexibleIterations = async () => {
+  const clusterArticlesFlexibleIterations = async () => {
     if (isLoadingFlexible) return;
     setIsLoadingFlexible(true);
     setTreeData([]);
@@ -75,7 +75,7 @@ function App() {
     ];
   };
 
-  const clusterBlogsHierarchical = async () => {
+  const clusterarticlesHierarchical = async () => {
     setClusters({});
     const response = await fetch("http://localhost:3000/clusters/hierarchical");
     const data = await response.json();
@@ -85,30 +85,30 @@ function App() {
 
   return (
     <>
-      <H1 text="Blogs available" />
-      <P text={blogs.join(" ♦ ")} />
+      <H1 text="Articles available" />
+      <P text={articles.join(" ♦ ")} />
 
       <div className="flex gap-2 mt-4 mb-4">
-        <Button onClick={clusterBlogsFixedIterations} disabled={isLoadingFixed}>
-          Cluster blogs fixed iterations (10)
+        <Button onClick={clusterArticlesFixedIterations} disabled={isLoadingFixed}>
+          Cluster articles fixed iterations (10)
         </Button>
         <Button
-          onClick={clusterBlogsFlexibleIterations}
+          onClick={clusterArticlesFlexibleIterations}
           disabled={isLoadingFlexible}
         >
-          Cluster blogs flexible iterations
+          Cluster articles flexible iterations
         </Button>
-        <Button onClick={clusterBlogsHierarchical}>
-          Cluster blogs hierarchically
+        <Button onClick={clusterarticlesHierarchical}>
+          Cluster articles hierarchically
         </Button>
       </div>
 
-      {Object.entries(clusters).map(([clusterName, blogs]) => (
+      {Object.entries(clusters).map(([clusterName, articles]) => (
         <div key={clusterName}>
           <H2 text={clusterName} className="-mb-2 !mt-2 border-b-0" />
           <ul>
-            {blogs.map((blog, index) => (
-              <li key={index}>{blog}</li>
+            {articles.map((article, index) => (
+              <li key={index}>{article}</li>
             ))}
           </ul>
         </div>
